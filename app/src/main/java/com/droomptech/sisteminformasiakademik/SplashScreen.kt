d4d549600423fa5e4b4f2f67b78f67c6a30d8df0
Tisna Hadiana <tisnahadiana@gmail.com>
2022-11-13 22:03:51 +0700

    Initial commit

---
+++ app/src/main/java/com/droomptech/sisteminformasiakademik/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.droomptech.sisteminformasiakademik
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
